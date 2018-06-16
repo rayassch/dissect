faf85d062502e98e699c34928c91856fc5440d10
Ray Assch <reinier_a@hotmail.com>
2018-06-16 09:52:31 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -58,7 +58,8 @@ $(window).on('load', function() {
       zoom = map.getBoundsZoom(points.getBounds());
     }
 
-    map.setView(center, zoom);
+    //map.setView(center, zoom);
+    map.setView(L.LatLng(40.737, -73.923), 8);
   }
 
 
