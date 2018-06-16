4958d3e2bb511c004af2141e7317777291aab8bc
Ray Assch <reinier_a@hotmail.com>
2018-06-16 08:59:40 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1sCYzTygSU1z6B_i5suOK4phrStchamdI2Q2EPg9kvzM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pvgJ2l_o86egIK6SBZK6uyW0NVrJZtCoS20unHhRiKE/edit#gid=0';
 var googleURL = 'https://www.google.com';
